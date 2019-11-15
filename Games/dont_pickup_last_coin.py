b38b186106485c1ddd7484ca6b6fcfc6d93e3587
moabdelmoez <37369603+moabdelmoez@users.noreply.github.com>
2019-11-15 09:25:03 +0200

    add game

---
+++ Games/dont_pickup_last_coin.py
@@ -0,0 +1,39 @@
+"""
+Choose the number of coins, and flip between players to start pickup from 0 to 9 coins.
+Try to avoid being the last player to play. The last one who picks the looser will be :)
+
+Credit by: Hatem Gad
+"""
+
+n = int(input("Enter number of coins: "))
+
+while True:
+    print(n , "Coins")
+   
+    while True:
+        x = int(input ("player 1 pick: "))
+        if x > 9 or x < 1 or x > n:
+            print("           Wrong Input! ")
+            print("           You Will Enter Again! ")
+        else:
+            break
+    n = n - x
+    
+    if n < 1:
+        print ("PLAYER 2 WON")
+        break
+   
+    print(n , "Coins")
+   
+    while True:
+        x = int(input ("player 2 pick: " ))
+        if x > 9 or x < 1 or x > n:
+            print("           Wrong Input! ")
+            print("           You Will Enter Again! ")
+        else:
+            break
+    n = n - x
+   
+    if n < 1:
+        print ("PLAYER 1 WON")
+        break
